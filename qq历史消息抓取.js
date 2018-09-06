556fdc2065b68c0aff4bd0d2254490ec5f9f752b
ridersam <e1399579@gmail.com>
2018-09-07 00:38:02 +0800

    暂存

--- qq历史消息抓取.js
+++ qq历史消息抓取.js
@@ -16,24 +16,35 @@ function initRecord(record, key) {
     }
 }
 
+var list_view = className("AbsListView").findOne();
+var unique_map = [];
 do {
     idMatches(regexp).find().forEach(function (o) {
         var text = o.text(); // 内容
         var text_id = o.id();
+
+        // 使用横坐标(left,right)+内容去重
+        var rect = o.bounds();
+        var unique_key = rect.centerX() + text;
+        if (-1 === unique_map.indexOf(unique_key)) {
+            unique_map.push(unique_key);
+        } else {
+            return;
+        }
         
         if (text_id === content_id) {
             var parent_ele = o.parent();
             // 3个元素则含时间元素
             var position = (parent_ele.childCount() > 2) ? 1 : 0;
-
+    
             // 头像在左边是他人
             var talk_text = 
                 (parent_ele.child(position).id() === "com.tencent.tim:id/chat_item_head_icon")
-                ? "OTHER:" : "I:";
+                ? "F:" : "I:";
             initRecord(record_mixed, current_date);
             record_mixed[current_date].push(talk_text + text);
-
-            if (talk_text === "OTHER:") {
+    
+            if (talk_text === "F:") {
                 initRecord(record_other, current_date);
                 record_other[current_date].push(text);
             } else {
@@ -42,23 +53,29 @@ do {
             }
         } else if (text_id === timestamp_id) {
             var arr = text.split(" ");
-            var date = arr[0];
-            var time = arr[1];
-
+            if (arr.length > 1) {
+                var date = arr[0];
+                var time = arr[1];    
+            } else {
+                var now = new Date();
+                var date = (now.getMonth() + 1) + '-' + now.getDate();
+                var time = arr[0];
+            }
+            
             if (date !== current_date) {
                 current_date = date;
                 initRecord(record_mixed, current_date);
                 initRecord(record_other, current_date);
                 initRecord(record_i, current_date);
             }
-
+    
             record_mixed[current_date].push(time);
             record_other[current_date].push(time);
             record_i[current_date].push(time);
         }
     });
     sleep(750);
-} while(className("AbsListView").findOne().scrollForward());
+} while(list_view.scrollForward());
 
 // log(record_mixed);
 // log(record_i);
@@ -67,11 +84,12 @@ do {
 function saveToFile(record, prefix) {
     for (let key in record) {
         var path = prefix + key + ".txt";
+        files.create(path);
         files.write(path, record[key].join("\r\n"));
     }
 }
 
-// saveToFile(record_mixed, "mixed_");
-// saveToFile(record_i, "i_");
-saveToFile(record_other, "other_");
+//saveToFile(record_mixed, "record/mixed_");
+//saveToFile(record_i, "record/i_");
+saveToFile(record_other, "record/firend_");
 
